884975232ea6df3352761b7282d0dddf9ef0a69f
Christian Ege <ch@ege.io>
2023-10-21 13:23:34 +0200

    fix: 📄 the SPDX license specifier
    Signed-off-by: Christian Ege <ch@ege.io>

--- dockerfiles/golang/bash_completion.sh
+++ dockerfiles/golang/bash_completion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Source https://serverfault.com/a/831184
-# License: CC BY-SA 4.0
+# SPDX-License-Identifier: CC-BY-SA-4.0
+# Origin: https://serverfault.com/a/831184
 
 for bcfile in ~/.bash_completion.d/* ; do
   # shellcheck source=/dev/null
